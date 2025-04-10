2438157bb550402eec123868dfabb3556c99469c
eber-cba <ebercoronel29@gmail.com>
2025-04-09 21:15:16 -0300

    db actualizado host

--- config/db.js
+++ config/db.js
@@ -2,11 +2,12 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   allowExitOnIdle: true,
+  ssl:
+    process.env.DB_HOST !== "localhost" ? { rejectUnauthorized: false } : false,
 });
 
 pool
